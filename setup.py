80e143350976066cf0fa32c04bfdecdfb8a6572c
Finbarr Brady <fibrady@cisco.com>
2020-05-01 08:39:48 +0100

    Bump version: 0.1.8 → 0.1.9

--- setup.py
+++ setup.py
@@ -46,6 +46,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/fbradyirl/vodafone_ie_account_checker',
-    version='0.1.8',
+    version='0.1.9',
     zip_safe=False,
 )
--- vodafone_ie_account_checker/__init__.py
+++ vodafone_ie_account_checker/__init__.py
@@ -2,7 +2,7 @@
 
 __author__ = """Finbarr Brady"""
 __email__ = 'finbarr.brady@gmail.com'
-__version__ = '0.1.8'
+__version__ = '0.1.9'
 
 
 import coloredlogs
