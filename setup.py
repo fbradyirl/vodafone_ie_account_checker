64deac435c8f5bfc9cdc583c766922a01e909364
Finbarr Brady <fibrady@cisco.com>
2020-05-01 12:40:52 +0100

    Bump version: 0.3.5 → 0.3.6

--- setup.py
+++ setup.py
@@ -45,6 +45,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/fbradyirl/vodafone_ie_account_checker',
-    version='0.3.5',
+    version='0.3.6',
     zip_safe=False,
 )
